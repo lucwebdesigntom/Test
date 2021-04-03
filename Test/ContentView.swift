6407999ade7c922a09408daa99d1b889bf6a1f85
Lukasz <ltomczyk1984@gmail.com>
2021-04-03 12:22:19 +0100

    Commit message

---
+++ Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by Lukasz Tomczyk on 03/04/2021.
+//  Copyright © 2021 Lukasz Tomczyk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
